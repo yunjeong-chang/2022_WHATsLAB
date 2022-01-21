425d11b4707a93552d1025dc8855f3c5e967925d
JEONGJANGEE <70889699+yunjeong-chang@users.noreply.github.com>
2022-01-22 01:20:24 +0900

    Update capture-csv.py

--- create_dataset/capture-csv.py
+++ create_dataset/capture-csv.py
@@ -103,9 +103,9 @@ for val in range(1, num_coords+1):
     landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
 
 #run only the first time and comment out
-#with open('dataset-metaverse.csv',mode='w',newline='') as f :
-#    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#    csv_writer.writerow(landmarks)
+with open('dataset-metaverse.csv',mode='w',newline='') as f :
+    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(landmarks)
     
 class_name = "run"
 flag = 0 
