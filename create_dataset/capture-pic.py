a56f53b2f4ac52bb9b4777dee4da66f78883597a
JEONGJANGEE <70889699+yunjeong-chang@users.noreply.github.com>
2022-01-22 01:18:39 +0900

    Update capture-pic.py

--- create_dataset/capture-pic.py
+++ create_dataset/capture-pic.py
@@ -10,7 +10,7 @@ k = ktb.Kinect()
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-os.makedirs('pose-dataset-0121', exist_ok=True)
+os.makedirs('pose-dataset', exist_ok=True)
 flag = 0 
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
